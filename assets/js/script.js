65780daa850340501ee6a8642996b9c3e3ca69dc
Daniela Onici <danielaonici@gmail.com>
2022-06-15 17:28:16 -0400

    including head comments according to the IT standards (Conestoga)

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// index.html
+//     PROG1800-22S-Sec3 - Assignment 2
+
+//     Dynamic website that collects information from a form to generate a receipt.
+
+//     Daniela Onici Student ID# 8754297, 2022.06.12: Created 
+//                                        2022.06.15: Finished
+
+
+var name = document.getElementById("name").value;
